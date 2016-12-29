10f3cf17e98db5cfc989a274e49cff0acdade030
darrennorris <doon75@hotmail.com>
2016-12-29 12:05:49 -0300

    update br319 data

--- R/data.R
+++ R/data.R
@@ -1,10 +1,9 @@
-#' Data from 9 PPBIO plots along the BR319 in Brazil.
+#' Data from 5 PPBIO plots along the BR319 in Brazil.
 #'
-#' A dataset containing the azimuth and length of segments from 9 plots:
-#' M01_TN_0500, M01_TN_3500, M02_TN_1500, M06_TS_0500, M02_TN_0500, M02_TN_2500,
-#' M02_TS_0500, M03_TN_2500, M09_TN_1500 .
+#' A dataset containing the azimuth and length of segments from 5 plots:
+#' "M01_TN_0500", "M02_TN_0500","M02_TN_2500","M03_TN_1500", "M03_TN_2500"
 #'
-#' @format A data frame with 264 rows and 6 variables:
+#' @format A data frame with 138 rows and 6 variables:
 #' \describe{
 #'   \item{aid}{unique rowid, for data checking.}
 #'   \item{plot_id}{ID code, unique for each plot.}
@@ -15,13 +14,12 @@
 #' }
 #' @source \url{https://ppbio.inpa.gov.br/sitios/br319/}
 "br319"
-#' Calculated areas from 9 PPBIO plots along the BR319 in Brazil.
+#' Calculated areas from 5 PPBIO plots along the BR319 in Brazil.
 #'
-#' A dataset containing the calculated area (m2) from 9 nonlinear plots:
-#' M01_TN_0500, M01_TN_3500, M02_TN_1500, M06_TS_0500, M02_TN_0500, M02_TN_2500,
-#' M02_TS_0500, M03_TN_2500, M09_TN_1500 .
+#' A dataset containing the calculated area (m2) from 5 nonlinear plots:
+#' "M01_TN_0500", "M02_TN_0500","M02_TN_2500","M03_TN_1500", "M03_TN_2500"
 #'
-#' @format A data frame with 864 rows and 8 variables:
+#' @format A data frame with 440 rows and 8 variables:
 #' \describe{
 #'   \item{.id}{ID code, unique for each plot.}
 #'   \item{variable}{Indicates which segments were used (4 types).}
@@ -34,13 +32,12 @@
 #' }
 #' 
 "br319res"
-#' Geographic coordiantes from 9 PPBIO plots along the BR319 in Brazil.
+#' Geographic coordiantes from 5 PPBIO plots along the BR319 in Brazil.
 #'
-#' A dataset containing geographic coordinates of the start point of 9 nonlinear plots:
-#' M01_TN_0500, M01_TN_3500, M02_TN_1500, M06_TS_0500, M02_TN_0500, M02_TN_2500,
-#' M02_TS_0500, M03_TN_2500, M09_TN_1500 .
+#' A dataset containing geographic coordinates of the start point of 5 nonlinear plots:
+#' "M01_TN_0500", "M02_TN_0500","M02_TN_2500","M03_TN_1500", "M03_TN_2500"
 #'
-#' @format A data frame with 3 rows and 3 variables:
+#' @format A data frame with 5 rows and 3 variables:
 #' \describe{
 #'   \item{PLOT_ID}{ID code, unique for each plot.}
 #'   \item{LONG}{Longitude (x), decimal degrees, WGS84, epsg=4326.}
